37172c5ca24766fd614550fb76ac6837f81f02bf
Victor de la C <vcg_cruz@hotmail.com>
2020-12-17 07:52:31 -0600

    project initial commit

---
+++ src/main/java/com/vcgdev/store/peperoni/PeperoniStRestApplication.java
@@ -0,0 +1,13 @@
+package com.vcgdev.store.peperoni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeperoniStRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeperoniStRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vcgdev/store/peperoni/PeperoniStRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vcgdev.store.peperoni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeperoniStRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
